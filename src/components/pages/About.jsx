b5d013618b12f7888835d48d7909ba8d56a7a97f
JLavvy <lavogajanyce@gmail.com>
2024-02-05 10:31:01 +0300

    update on the about section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,21 +1,25 @@
-
-import React from 'react'
-import { FaGraduationCap, FaRegClock } from 'react-icons/fa'
-import "./About.css"
+import React from 'react';
+import { FaGraduationCap, FaRegClock } from 'react-icons/fa';
+import './About.css';
 
 export const About = () => {
   return (
     <section id='about'>
-      <p className="about-text">Get to Know more</p>
-      <h1 className="about-title">About me</h1>
+      <div className="about-text-container">
+        <p className="about-text">Get to Know more</p>
+        <h1 className="about-title">About me</h1>
+      </div>
+
       <div className="about-container">
         <div className="about-profile">
-          <img src="./src/assets/FAN_2730 copy.JPG" alt="Profile Picture" className="about-pic" />
+          <img
+            src="./src/assets/FAN_2730 copy.JPG"
+            alt="Profile Picture"
+            className="about-pic"
+          />
         </div>
         <div className="about-container-details">
-
           <div className="about-containers">
-
             <div className="details-container">
               <FaRegClock className="icon" onClick={Location} />
               <h3>Experience</h3>
@@ -27,21 +31,13 @@ export const About = () => {
               <h3>Education</h3>
               <p>B.SC. Bachelors Degree In<br /> Computer Science</p>
             </div>
-
           </div>
+
           <div className="text-container">
-            <p>I'm a recent Computer Science graduate with a passion for crafting engaging and user-friendly front-end experiences for websites and web applications. My journey in web development has equipped me with a solid foundation and a zest for creating seamless, impactful user interfaces.
-              Explore some of my recent projects in the Projects section to get a glimpse of my skills and dedication. Each project is a step towards contributing to the success of the overall product.
-              Beyond coding, I enjoy sharing insights and knowledge I've gained throughout my journey in web development. Let's connect and learn from each other!
-              I'm currently seeking exciting job opportunities where I can apply my skills, continue learning, and contribute to meaningful projects. If you have an opportunity that aligns with my passion and skills, feel free to reach out. I'm eager to embark on the next phase of my career journey. 🚀</p>
+            <p>I'm a recent Computer Science graduate with a passion for crafting engaging and user-friendly front-end experiences for websites and web applications. My journey in web development has equipped me with a solid foundation and a zest for creating seamless, impactful user interfaces. Explore some of my recent projects in the Projects section to get a glimpse of my skills and dedication. Each project is a step towards contributing to the success of the overall product. Beyond coding, I enjoy sharing insights and knowledge I've gained throughout my journey in web development. Let's connect and learn from each other! I'm currently seeking exciting job opportunities where I can apply my skills, continue learning, and contribute to meaningful projects. If you have an opportunity that aligns with my passion and skills, feel free to reach out. I'm eager to embark on the next phase of my career journey. 🚀</p>
           </div>
         </div>
-
       </div>
-
-      
-
     </section>
-  )
-}
-
+  );
+};
